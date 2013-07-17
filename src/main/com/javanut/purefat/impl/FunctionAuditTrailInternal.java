faae8df9e20125db479bd28024f20d8922b11cf8
nate <nate@727a7f19-0e16-0648-901b-2fef1cee80f5>
2013-07-17 04:12:42 +0000

    BSD 2 Copyright and bitcoin address
    git-svn-id: svn+ssh://192.168.111.11/var/svn/javanut/project/pureFunctionAuditTrail@2035 727a7f19-0e16-0648-901b-2fef1cee80f5

--- src/main/com/javanut/purefat/impl/FunctionAuditTrailInternal.java
+++ src/main/com/javanut/purefat/impl/FunctionAuditTrailInternal.java
@@ -1,3 +1,34 @@
+/**
+ * Copyright (c) 2013, Nathan Tippy
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * 
+ * @author  Nathan Tippy <tippyn@ociweb.com>
+ * bitcoin:1NBzAoTTf1PZpYTn7WbXDTf17gddJHC8eY?amount=0.01&message=PFAT%20donation
+ *
+ */
 package com.javanut.purefat.impl;
 
 import java.lang.ref.WeakReference;
@@ -15,9 +46,9 @@ import org.slf4j.helpers.MessageFormatter;
 
 public class FunctionAuditTrailInternal implements FunctionAuditTrail  {
     
-    
-    private final static int RING_BUFFER_SIZE_DEFAULT = 1<<24;//16M;
-    private final static String RING_BUFFER_SIZE_KEY = "purefat.ringbuffer.size";
+    //Do not make this too much larger it will grow as needed.
+    private final static int RING_BUFFER_INITIAL_SIZE = 1<<20;//1M elements;
+    private final static String RING_BUFFER_INITIAL_SIZE_KEY = "purefat.ringbuffer.size";
     
     private final static boolean RING_BUFFER_GROW_DEFAULT = true;
     private final static String RING_BUFFER_GROW_KEY = "purefat.ringbuffer.grow";
@@ -37,9 +68,9 @@ public class FunctionAuditTrailInternal implements FunctionAuditTrail  {
     private boolean                shouldGrowArray = RING_BUFFER_GROW_DEFAULT;
 
     private final Map<String, Map<String,FunMetaData>> functionMeta = new HashMap<String,Map<String,FunMetaData>>();
-    
+
     public FunctionAuditTrailInternal() {
-        this(Integer.parseInt(System.getProperty(RING_BUFFER_SIZE_KEY, Integer.toString(RING_BUFFER_SIZE_DEFAULT))),
+        this(Integer.parseInt(System.getProperty(RING_BUFFER_INITIAL_SIZE_KEY, Integer.toString(RING_BUFFER_INITIAL_SIZE))),
              Boolean.parseBoolean(System.getProperty(RING_BUFFER_GROW_KEY, Boolean.toString(RING_BUFFER_GROW_DEFAULT)))
              );
     }
